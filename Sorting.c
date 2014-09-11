d637af92877609d0cc65435aa4cf7a141eac1277
Hampus Bergstrom <habe5318@geijer.it.uu.se>
2014-09-11 14:36:42 +0200

    able to read the header of an mp3 file

--- Sorting.c
+++ Sorting.c
@@ -1,31 +1,50 @@
 #include <stdio.h>
 
-struct Header_struct {
-  char id[5];
-  char flag;
-  char size[4];
-};
-
+char header[10];
+int size = 0;
 FILE *infile;
-
 
 int main(void) {
   infile = fopen("music0.mp3", "r");
   if (infile == NULL) {
     puts("File not found");
   }
+  
+  char ch;
 
-  //struct Header_struct h;
-  int ch;
-  //fread(&h,sizeof(h),1,infile);
-  for (int i = 0; i < 10; ++i){
-
+  for (int i = 0; i < 10; i++){
     ch = getc(infile);
+    header[i] = ch;
     printf("%d ",ch);
-    fseek(infile, 0,SEEK_CUR);
-    // ch = getc(infile);
-    //printf("%c\n",ch);
   }
-  //    printf("id: %c%c%c%c%c\nflag:%c\nsize:%c%c%c%c\n",h.id[0],h.id[1],h.id[2],h.id[3],h.id[4],h.flag, h.size[0], h.size[1], h.size[2], h.size[3]);
+  
+  for (int i = 5; i < 10; i++){
+    header[i] = header[i];
+    size += header[i];
+    if (i < 9)
+      size = size << 7;
+    
+  }
+ 
+  int body[size];
+
+  for (int i = 10; i < size; ++i){
+    body[i] = getc(infile);
+    printf("%d", body[i]);
+  
+   }
+
 }
+    // CH = getc(infile);
+    //printf("%c\n",ch);
+    /*
+    for(int i = 5; i < 10; i++){
+      size += sizeOfFile[i];
+      size = size << 7;
+    */
+    
+  //    fseek(infile, 0,SEEK_CUR);
+    
+  //    printf("id: %c%c%c%c%c\nflag:%c\nsize:%c%c%c%c\n",h.id[0],h.id[1],h.id[2],h.id[3],h.id[4],h.flag, h.size[0], h.size[1], h.size[2], h.size[3]);
+
 
