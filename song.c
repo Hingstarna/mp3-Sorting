aff8030f3cd1961058b83ee593ad58780d55db62
Paow <micaelloberg@gmail.com>
2014-09-18 11:53:46 +0200

    stuff

--- song.c
+++ song.c
@@ -15,13 +15,23 @@ void destroy_song(song *s) {
   free(s->album);
   free(s->title);
   free(s->year);
-  free(s);
+  free(s); 
+}
+void set_artist(song *s, char *string){
+  *(s->artist) = *string;
+}
+void set_album(song *s, char *string){
+  *(s->album) = *string;
+}
+void set_title(song *s, char *string){
+  *(s->title) = *string;
+}
+void set_year(song *s, char *string){
+  *(s->year) = *string;
 }
 
-void set_artist(song *s){}
-void set_album(song *s){}
-void set_title(song *s){}
-void set_year(song *s){}
+
+
 
 char * get_artist(song *s) {
   return s->artist;
